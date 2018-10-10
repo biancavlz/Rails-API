59403ea6ef01870fefad6cf93576bae91be601e9
Bianca <ing_biank@hotmail.com>
2018-10-10 13:14:06 +0200

    creates multiples attributes for article Factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    sequence(:title) { |n| "My awesome Article #{n}" }
+    sequence(:content) { |n| "My awesome Content #{n}" }
+    sequence(:slug) { |n| "my-awesome-slug-#{n}" }
   end
 end
