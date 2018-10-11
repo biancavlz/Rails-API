e2604c0e0746905275f798aeca25ab35e21dc0be
Bianca <ing_biank@hotmail.com>
2018-10-11 15:14:37 +0200

    Creates support directory for api helpers and configures them and refactor articles spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,29 +2,23 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe '#index' do
+    subject { get :index }
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
-      create_list :article, 2
-      get :index
+      articles = create_list :article, 2
+      subject
 
-      json = JSON.parse(response.body)
-      json_data = json['data']
-      expect(json_data.length).to eq(2)
-      expect(json_data[0]['attributes']).to eq({
-        "title" => "My awesome Article 1",
-        "content" => "My awesome Content 1",
-        "slug" => "my-awesome-slug-1"
-      })
-
-      expect(json_data[1]['attributes']).to eq({
-        "title" => "My awesome Article 2",
-        "content" => "My awesome Content 2",
-        "slug" => "my-awesome-slug-2"
-      })
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+          "title" => article.title,
+          "content" => article.content,
+          "slug" => article.slug
+        })
+      end
     end
   end
 end
