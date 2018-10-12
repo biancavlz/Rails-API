8a9da50f533c2237ff3d79e1af67aa06fe084125
Bianca <ing_biank@hotmail.com>
2018-10-12 21:26:03 +0200

    Creates recent scope to get the articles in descendant order

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :slug, presence: true, uniqueness: true
+
+  scope :recent, -> { order(created_at: :desc) }
 end
