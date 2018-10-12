a663f83152025118d77b3f3d609637d5d445bec9
Bianca <ing_biank@hotmail.com>
2018-10-12 21:28:28 +0200

    Gets articles for index action in descendant order through recent scope

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,17 +2,18 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe '#index' do
-    subject { get :index }
+    subject(:get_index) { get :index }
+
     it 'should return success response' do
-      subject
+      get_index
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
-      articles = create_list :article, 2
-      subject
+      create_list :article, 2
+      get_index
 
-      articles.each_with_index do |article, index|
+      Article.recent.each_with_index do |article, index|
         expect(json_data[index]['attributes']).to eq({
           "title" => article.title,
           "content" => article.content,
@@ -20,5 +21,15 @@ describe ArticlesController do
         })
       end
     end
+
+    it 'should return articles in the proper order' do
+      old_article = create :article
+      newer_article =  create :article
+      get_index
+
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
+    end
+
   end
 end
