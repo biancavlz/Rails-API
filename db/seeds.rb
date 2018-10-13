cb9fb2b1941273082de3ad4090a636a0e40ce3bf
Bianca <ing_biank@hotmail.com>
2018-10-13 10:41:02 +0200

    Populates development db, adds 10 articles to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |article|
+  Article.create!(
+    title: "Title #{article}",
+    content: "Sed ut perspiciatis unde omnis iste
+    natus error sit voluptatem accusantium doloremque
+    laudantium, totam rem aperiam, eaque ipsa quae ab
+    illo inventore veritatis et quasi architecto beatae
+    vitae dicta sunt explicabo.",
+    slug: "title-#{article}"
+  )
+end
+
+puts "Created 3 articles"
