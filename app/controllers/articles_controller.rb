0ed2390af06ec602485c4e77b2501b912a086a22
Bianca <ing_biank@hotmail.com>
2018-10-14 12:28:56 +0200

    Fix per_page naming

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    articles = Article.recent.page(params[:page]).per(params[:per_page])
+    articles = Article.recent.page(params[:page]).per_page(params[:per_page])
     render json: articles
   end
 
