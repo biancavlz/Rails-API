4a77f7c83901de55c6eb5538e134eaafd938eb0a
Bianca <ing_biank@hotmail.com>
2018-11-19 13:19:44 +0100

    Authentication service success

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -3,13 +3,44 @@ require 'user_authenticator'
 
 describe 'UserAuthenticator' do
   describe '#perform' do
+    let(:authenticator) { UserAuthenticator.new('sample_code') }
+    subject(:perform) { authenticator.perform }
+    
     context 'when the code is incorrect' do
+      let(:error) { double('Sawyer::Resource', error: 'bad_verification_code') }
+      
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return(error)
+      end
+      
       it 'should raise an error' do
-        authenticator = UserAuthenticator.new('sample_code')
-        expect{ authenticator.perform }.to raise_error(
+        expect{ perform }.to raise_error(
           UserAuthenticator::AuthenticationError
         )
         expect(authenticator.user).to be_nil
+      end
+    end
+
+    context 'when the code is correct' do
+      let (:user_data) do {
+          login: 'jsmith1', 
+          avatar_url: 'http://myexample.com/avatar',
+          url: 'http://myexample.com',
+          name: 'John Smith'
+        }
+      end
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(
+          :exchange_code_for_token).and_return('validaccesstoken')
+
+        allow_any_instance_of(Octokit::Client).to receive(
+          :user).and_return(user_data)
+      end
+
+      it 'should save the user when does not exist' do
+        expect{ perform }.to change{ User.count }.by(1)
+        expect(User.last.name).to eq 'John Smith'
       end
     end
   end
