e854412a68268748ec546a29c65744ce9dd640d5
Bianca <ing_biank@hotmail.com>
2018-11-19 13:46:13 +0100

    Reusing existed users

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -42,6 +42,13 @@ describe 'UserAuthenticator' do
         expect{ perform }.to change{ User.count }.by(1)
         expect(User.last.name).to eq 'John Smith'
       end
+
+      it 'should reuse already registered user' do
+        user = create :user, user_data
+
+        expect{ subject }.not_to change { User.count }
+        expect(authenticator.user).to eq(user)
+      end
     end
   end
 end
