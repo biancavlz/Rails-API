b28fbee47d9d3a8b29ee0268f5f0dc7621a33de2
Bianca <ing_biank@hotmail.com>
2018-11-20 11:50:44 +0100

    fix for generation of token once

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -2,18 +2,14 @@ class AccessToken < ApplicationRecord
   validates :token, presence: true, uniqueness: true
   
   belongs_to :user
-  after_initialize :set_access_token
+  after_initialize :generate_token
 
   private
 
-  def set_access_token
-    self.token = generate_token
-  end
-
   def generate_token
     loop do
-      token = SecureRandom.hex(10)
-      break token unless User.where(access_token: token).exists?
+      break if token.present? && !AccessToken.where.not(id: id).exists?(token: token)
+      self.token = SecureRandom.hex(10)
     end
   end
 end
