e017d60e2d1edc5565e3d7cf82d36e36a944fb7a
Bianca <ing_biank@hotmail.com>
2018-11-21 12:14:36 +0100

    Adds articles association with user and does not allow to updates an articles from owner

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -156,7 +156,10 @@ describe ArticlesController do
   end
 
   describe '#update' do
-    let(:article) { create :article }
+    let(:user) { create :user }
+    let(:article) { create :article, user: user }
+    let(:access_token) { user.create_access_token }
+    
     subject { patch :update, params: {id: article.id} }
 
     context 'when no code provided' do
@@ -168,10 +171,19 @@ describe ArticlesController do
       it_behaves_like 'forbidden_requests'
     end
 
-    context 'when authorized' do
-      let(:user) { create :user }
-      let(:access_token) { user.create_access_token }
+    context 'when trying to update not own article' do
+      let(:other_user) { create :user }
+      let(:other_article) { create :article, user: other_user }
+
+      subject { patch :update, params: { id: other_article.id } }
       
+      before { request.headers['authorization'] = "Bearer #{ access_token.token }" }
+
+      it_behaves_like 'forbidden_requests'
+
+    end
+
+    context 'when authorized' do  
       before { request.headers['authorization'] = "Bearer #{ access_token.token }" }
       
       context 'when invalid parameters provided' do
