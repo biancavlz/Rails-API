aa6bad6363658399eac868c9f028c54bd4f2237b
Bianca <ing_biank@hotmail.com>
2018-11-21 13:23:32 +0100

    Customise  index and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :comments
   post 'login', to: 'access_tokens#create'
   delete 'logout', to: 'access_tokens#destroy'
-  resources :articles
+  resources :articles do
+    resources :comments, only: [:index, :create]
+  end
 end
